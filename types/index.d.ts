51ce0b10209a8ce811aacbfce43a95fa68a1bf96
huayifeng <461249104@qq.com>
2019-01-26 17:30:29 +0800

    style: .d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,18 @@
 /**
- * @export
  * @desc return pkg context
- * @param {string} [url]
+ * @param {string} [url] package.json path
  * @returns {Object}
  */
 export declare function pkgRead(url?: string): Object;
 /**
- * @export
  * @desc update your pkg
- * @param {string} url
- * @param {Object} data
+ * @param {string} url package.json path
+ * @param {Object} data cover data
  */
 export declare function pkgUpdate(url: string, data: Object): void;
 /**
- * @export
  * @desc get path form your workSpace
- * @param {string} [url]
+ * @param {string} [url] package.json path
  * @returns {string}
  */
 export declare function pkgPath(url?: string): string;
